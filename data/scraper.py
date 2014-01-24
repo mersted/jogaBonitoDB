98f7a81c9d121039969d5361ab36739d126bd2c7
SimplyFaisal <d.f.animashaun@gmail.com>
2014-01-23 22:34:12 -0500

    refactored scraper.[y

--- data/scraper.py
+++ data/scraper.py
@@ -1,31 +1,19 @@
 import urllib.request
+from os import getcwd
+from os import sep
+from os import mkdir 
 
 #http://www.football-data.co.uk/mmz4281/1314/D1.csv
 countries =  ["England","Germany","Spain","Italy","France"]
 
 
-season = ["1314",
-         "1213",
-         "1112",
-         "1112",
-         "1011",
-         "0910",
-         "0809",
-         "0708",
-         "0607",
-         "0506",
-         "0405",
-         "0304",
-         "0203",
-         "0102",
-         "0001",
-         "9900",
-         "9899",
-         "9798",
-         "9697",
-         "9596",
-         "9495",
-         "9394"]
+season = ["1314","1213","1112","1112","1011","0910",
+         "0809","0708","0607","0506","0405","0304",
+         "0203","0102","0001","9900","9899","9798",
+         "9697","9596","9495","9394"]
+
+directory = getcwd()
+
 
 def query(locale): #takes in the country and scrapes that countries data
     if locale == "England":
@@ -38,26 +26,24 @@ def query(locale): #takes in the country and scrapes that countries data
         xlc = "SP1"
     elif locale == "France":
         xlc = "F1"
+    
+    newdir = directory + sep + locale + sep #generates a filepath in the form cwd/country/
+    mkdir(newdir)    
         
-
     for year in season: #downloads csv files for each year in the list season
-        site = "http://www.football-data.co.uk/mmz4281/" + str(year) + "/" +str(xlc)+ ".csv" #generates the final url 
-        request = urllib.request.Request(site)
-        response = urllib.request.urlopen(site)
-        page = response.read()
-        text = str(page)
-        fileName = locale + str(year)+ ".csv"
-
-        handle = open(fileName,"w")
-        handle.write(text)
-        print("Download Done for {0}, {1}".format(locale,year)
+        site = "http://www.football-data.co.uk/mmz4281/" + str(year) + "/" +str(xlc)+ ".csv" #generates the request url
+        fpath = newdir + year + ".csv" #the new name for the corresponding csv file
+        request = urllib.request.urlretrieve(site,fpath)
+        print("Download Done for {0}, {1}".format(locale,year))
+     
     print("Finally Done")
 
-##query("Germany")
-##query("England")
-#query("Spain")
-#query("France")
-#query("Italy")
+
+query("Germany")
+query("England")
+query("Spain")
+query("France")
+query("Italy")
 
     
             
